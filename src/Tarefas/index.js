b10ba435148711eec2c032790807ef8d9c92053e
Beatriz Ferreira dos Santos <BFSantos@raiadrogasil.com>
2020-06-02 15:06:41 -0300

    primeiro commit

---
+++ src/Tarefas/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Tarefas extends Component{
+
+    state = {
+        products: []
+    }
+
+    findProducts(){
+        
+    }
+
+
+    render(){
+
+    }
+
+}
+
